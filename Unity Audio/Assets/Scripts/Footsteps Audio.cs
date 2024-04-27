92c926e105e795f1921c5a398e8e067e0335e7f7
Mario Garcia Sutil <mariosutil2003@gmail.com>
2024-04-27 22:04:47 +0200

    Footsteps audio logic

--- Unity Audio/Assets/Scripts/Footsteps Audio.cs
+++ Unity Audio/Assets/Scripts/Footsteps Audio.cs
@@ -1,34 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+
 
 public class FootstepsAudio : MonoBehaviour
 {
+    public AudioSource audioSource;
     public AudioClip[] metalFootsteps;
-
     public AudioClip[] concreteFootsteps;
 
-    // Start is called before the first frame update
+    private int rngfootsep;
+
     void Start()
     {
-        
+        rngfootsep = 0;
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Untagged")
         {
             //Audio de asfalto
+            rngfootsep = Random.Range(0, concreteFootsteps.Length);
+            audioSource.PlayOneShot(concreteFootsteps[rngfootsep]);
         }
 
         if (other.tag == "Metal")
         {
             //Audio de metal
+            Random.Range(0, metalFootsteps.Length);
+            audioSource.PlayOneShot(metalFootsteps[rngfootsep]);
         }
     }
 }
